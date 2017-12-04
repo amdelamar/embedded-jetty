0b1bc2e4b5ad6a019264dedde24a931a55b5291d
amdelamar <amdelamar@protonmail.com>
2017-12-04 15:22:58 -0500

    Add HTTP/2 server

--- src/main/java/hello/HelloServlet.java
+++ src/main/java/hello/HelloServlet.java
@@ -7,16 +7,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HelloServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
+    private static final Logger logger = LoggerFactory.getLogger(Http2Server.class);
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
+
+        String hello = "Hello from Jetty!";
+        String content = request.getProtocol() + " | " + 
+                request.getScheme().toUpperCase();
+        
+        logger.info(hello+" "+content);
+        
         response.getWriter()
-                .println("<div style=\"padding:50px;text-align:center;\"><h1>Hello from HelloServlet!</h1></div>");
+                .println("<div style=\"padding:50px;text-align:center;\"><h1>"+hello+"</h1><p><code>" + content
+                        + "</code></p></div>");
     }
 
 }
--- src/main/java/hello/Http2Server.java
+++ src/main/java/hello/Http2Server.java
@@ -1,6 +1,9 @@
 package hello;
 
+import org.eclipse.jetty.alpn.server.ALPNServerConnectionFactory;
 import org.eclipse.jetty.http.HttpScheme;
+import org.eclipse.jetty.http2.HTTP2Cipher;
+import org.eclipse.jetty.http2.server.HTTP2ServerConnectionFactory;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.HttpConfiguration;
 import org.eclipse.jetty.server.HttpConnectionFactory;
@@ -15,9 +18,9 @@ import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class HttpsServer {
+public class Http2Server {
 
-    private static final Logger logger = LoggerFactory.getLogger(HttpsServer.class);
+    private static final Logger logger = LoggerFactory.getLogger(Http2Server.class);
 
     public static void main(String[] args) throws Exception {
 
@@ -36,7 +39,11 @@ public class HttpsServer {
 
         // Protocols
         HttpConnectionFactory http = new HttpConnectionFactory(httpConfig);
-        HttpConnectionFactory https = new HttpConnectionFactory(httpsConfig);
+        HTTP2ServerConnectionFactory h2 = new HTTP2ServerConnectionFactory(httpsConfig);
+
+        // Protocol Negotiation
+        ALPNServerConnectionFactory alpn = new ALPNServerConnectionFactory();
+        alpn.setDefaultProtocol(http.getProtocol()); // sets default protocol to HTTP 1.1
 
         // Setup Threadpool
         QueuedThreadPool threadPool = new QueuedThreadPool();
@@ -56,18 +63,19 @@ public class HttpsServer {
         ssl.setKeyManagerPassword("changeit");
         ssl.setTrustStorePath(workingDir + "/deploy/keystore.jks");
         ssl.setTrustStorePassword("changeit");
+        ssl.setCipherComparator(HTTP2Cipher.COMPARATOR);
         ssl.setUseCipherSuitesOrder(true);
         ssl.setIncludeProtocols("TLSv1.2");
         ssl.setExcludeProtocols("TLSv1", "TLSv1.1"); // force tls1.2
         ssl.setExcludeCipherSuites("(MD5|DES|SHA|SHA1)$"); // dont allow weak ciphers
 
         // Add Connectors
-        ServerConnector connector = new ServerConnector(server, http);
+        ServerConnector connector = new ServerConnector(server, http, h2);
         connector.setName("unsecured");
         connector.setPort(8080);
         server.addConnector(connector);
 
-        ServerConnector connector2 = new ServerConnector(server, ssl, https);
+        ServerConnector connector2 = new ServerConnector(server, ssl, alpn, h2, http);
         connector2.setName("secured");
         connector2.setPort(8443);
         server.addConnector(connector2);
