46af29514438034ae66165441c190607e3e84e72
amdelamar <amdelamar@protonmail.com>
2017-12-04 10:33:04 -0500

    Add HelloServlet

---
+++ src/main/java/hello/HelloServlet.java
@@ -0,0 +1,22 @@
+package hello;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter()
+                .println("<div style=\"padding:50px;text-align:center;\"><h1>Hello from HelloServlet!</h1></div>");
+    }
+
+}
